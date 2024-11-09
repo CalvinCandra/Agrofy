9902ff88896090afe603309b8005538c200b0b61
IGLOW2004 <gungwiyana@gmail.com>
2024-11-09 10:24:22 +0800

    edit button component

--- src/components/Button/ButtonHref.jsx
+++ src/components/Button/ButtonHref.jsx
@@ -1,5 +1,5 @@
 export default function ButtonHref(props) {
-  const { text, href = "#", variant, onClick } = props;
+  const { text, href = "#", variant, onClick = "" } = props;
   const variants = {
     primary:
       "w-full m-1 text-white bg-main-green border border-main-green hover:bg-main-green-hover focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center",
